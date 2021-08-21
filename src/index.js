e0f69ece06ff67d166830dba0cecbed6e2ce2bdb
xnapisatel <85700886+xnapisatel@users.noreply.github.com>
2021-08-21 21:59:04 +0300

    docs: update description of  method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+    let result = 0;
+    if ( n <0 ){
+        n = n * (-1);
+    }
+    while (n) {
+      result = result * 10 + n % 10;
+      n = Math.floor(n / 10);
+    }
   
-}
+    return result;
+  }
+  
+  
+  
+  
